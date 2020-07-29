9c334304cb94d65541636f95cb24834005d88201
MBogushefsky <mbogushe@asu.edu>
2020-07-29 10:23:41 -0700

    Progress check-in

---
+++ Backend/Monier/obj/Release/Package/PackageTmp/SQL/CreateTableTransaction.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE `monier`.`transaction` (
+  `ID` VARCHAR(36) NOT NULL,
+  `UserID` VARCHAR(36) NOT NULL,
+  `PlaidAccountID` VARCHAR(64) NOT NULL,
+  `MerchantName` VARCHAR(255) NULL,
+  `Name` VARCHAR(255) NOT NULL,
+  `Amount` DECIMAL(15,2) NOT NULL,
+  `Pending` BIT(1) NOT NULL,
+  `PaymentChannel` VARCHAR(45) NOT NULL,
+  `Categories` VARCHAR(255) NULL,
+  `Date` DATE NOT NULL,
+  PRIMARY KEY (`ID`),
+  UNIQUE INDEX `idtransaction_UNIQUE` (`ID` ASC) VISIBLE);
+  ALTER TABLE `monier`.`transaction` 
+CHANGE COLUMN `Date` `TransactionDate` DATE NOT NULL ;
+ALTER TABLE `monier`.`transaction` 
+CHANGE COLUMN `Amount` `CostAmount` DECIMAL(15,2) NOT NULL ;
+ALTER TABLE `monier`.`transaction` 
+ADD COLUMN `PlaidTransactionID` VARCHAR(64) NOT NULL AFTER `ID`,
+ADD UNIQUE INDEX `PlaidTransactionID_UNIQUE` (`PlaidTransactionID` ASC) VISIBLE;
+;
